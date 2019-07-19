b834381a413285ac35268ba24b192e9a29c7666e
dmitrymail_k <dmitrymail_k@mail.ru>
2019-07-19 20:00:25 +0400

    Updates

---
+++ precache-manifest.9d1526e670179dca3e95e7aef9e7d3a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e927cb6fdede656cb03eca6794ef2f",
+    "url": "/template-boilerplate/index.html"
+  },
+  {
+    "revision": "0d5b04894ba0a2724831",
+    "url": "/template-boilerplate/static/js/2.f6663065.chunk.js"
+  },
+  {
+    "revision": "55aa1cf327088b5a4be3",
+    "url": "/template-boilerplate/static/js/main.dddbb130.chunk.js"
+  },
+  {
+    "revision": "2dd6e6e5e1f998dd2eb2",
+    "url": "/template-boilerplate/static/js/runtime~main.fab2e987.js"
+  },
+  {
+    "revision": "07bf6f0f7d5dd64829822e95e97f908d",
+    "url": "/template-boilerplate/static/media/load.07bf6f0f.gif"
+  }
+]);
